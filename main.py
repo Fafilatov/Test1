18921aef5c28e9d05b6ff389e6d640c9fbda8a42
Fafilatov <fafilatov@gmail.com>
2022-01-22 14:06:18 +0300

    vgncg-3

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def print_hi(name):
 
 # ghh?
 #dssdghjsduhjgshujpghus
+#wsefjaeoifjopj
+# @sdfigjsodigf
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
